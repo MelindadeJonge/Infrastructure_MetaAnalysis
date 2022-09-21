badeb87595721eb7c0adbdd881d0ee78ef43fc36
MelindadeJonge <melindadejonge@gmail.com>
2022-09-21 22:57:48 +0200

    Scripts to fit distance models added

---
+++ R/FitDistanceModels.R
@@ -0,0 +1,75 @@
+# Author: Melinda de Jonge
+# Date: 20-09-2022
+# Department of Environmental Science, Radboud University Nijmegen
+# e-mail: melinda.dejonge@ru.nl
+
+## -------------------------------------------------------------------------------------------------
+#'
+##--------------------------------------------------------------------------------------------------
+
+library(metafor)
+library(ggplot2)
+library(ggpubr)
+source(file.path('R','Functions.R'))
+
+# Load prepared data
+load(file.path('Data','Processed','GroupedData_main.RData'))
+load(file.path('Data','Processed','VarCovarMatrices_main.RData'))
+
+## ----- FIT MODELS --------------------------------------------------------------------------------
+REList = list(Mammals=list(~1|ID, ~1|Order/SpeciesCode, ~1|Source/Study),
+              Birds=list(~1|ID, ~1|Order/SpeciesCode, ~1|Source/Study),
+              Reptiles=list(~1|ID, ~1|Family/SpeciesCode, ~1|Source/Study),
+              Amphibians=list(~1|ID, ~1|Family/SpeciesCode, ~1|Source/Study))
+
+FEList = list(c(~1),
+           c(~logD),
+           c(~logD + logD2))
+
+modelNames= c('intercept','distanceLinear','distanceQuadratic')
+
+for(group in 1:length(DataList)) {
+  for(i in 1:3) {
+    m = rma.mv(logRR, V=VarCovarList[[group]],
+               mods=FEList[[i]][[1]],
+               random=REList[[group]],
+               data=DataList[[group]])
+    filename = file.path('Output','Models',
+                         paste0(names(DataList)[[group]],'_',modelNames[i],'_main.RData'))
+    save(m, file=filename)
+  }
+}
+
+## ----- Plot quadratic models ---------------------------------------------------------------------
+# Load the quadratic model for each group
+load(file.path('Output','Models','Mammals_distanceQuadratic_main.RData'))
+mammal.modelDistance = m
+load(file.path('Output','Models','Birds_distanceQuadratic_main.RData'))
+bird.modelDistance = m
+load(file.path('Output','Models','Reptiles_distanceQuadratic_main.RData'))
+reptile.modelDistance = m
+load(file.path('Output','Models','Amphibians_distanceQuadratic_main.RData'))
+amphibian.modelDistance = m
+
+# Plotting
+pm = distancePlots(mammal.modelDistance,
+                   data=DataList[[1]], title='a) Mammals')
+pb = distancePlots(bird.modelDistance,
+                   data=DataList[[2]], title='b) Birds')
+pr = distancePlots(reptile.modelDistance,
+                   data=DataList[[3]], title='c) Reptiles')
+pa = distancePlots(amphibian.modelDistance,
+                   data=DataList[[4]], title='d) Amphibians')
+
+fig2 = ggarrange(pm, pb, pr, pa,
+                 ncol=2, nrow=2, align='v')
+
+png(filename=file.path('Figures','Fig2.png'), width=4, height=4,
+    units='in',res=600)
+plot(fig2)
+dev.off()
+
+cairo_pdf(file=file.path('Figures','Fig2.pdf'), width=4, height=4)
+plot(fig2)
+dev.off()
+
--- R/Functions.R
+++ R/Functions.R
@@ -26,6 +26,16 @@ calc.vRRDelta <- function(x) {
   return(v)
 } 
 
+
+## ---- Calculate marginal and conditional R2 ------------------------------------------------------
+R2.func <- function(model) {
+  fixvar <-model$QM / model$k
+  R2m<-fixvar / (fixvar + sum(model$sigma2))
+  R2c<-(sum(model$sigma2) - model$sigma2[1] + fixvar) / (fixvar+sum(model$sigma2))
+  x <- c(R2m,R2c)
+  return(x)
+}
+
 ## ---- Figures ------------------------------------------------------------------------------------
 createMap <- function(X, title) {
   map_theme = theme(panel.grid.major = element_blank(),
@@ -66,5 +76,59 @@ createMap <- function(X, title) {
     map_theme
   
   return(g1)
+}
+
+
+distancePlots <- function(model, data, title) {
+  science_theme = theme(panel.grid.major = element_blank(),
+                        axis.title.y = element_text(vjust=1.25, size=8),
+                        axis.title.x = element_text(vjust=-0.3, size=8),
+                        axis.line = element_line(size=0.7, color="black"),
+                        axis.text.y = element_text(size=8),
+                        axis.text.x = element_text(size=8),
+                        legend.title = element_blank(),
+                        legend.position = 'right',
+                        legend.direct = 'vertical',
+                        plot.title = element_text(size=8))
+
+  # Get the marginal and conditional R2 of the model
+  R2 = round(R2.func(model), 2)
+
+  # Create predictions with 95% confidence intervalls
+  predDistance = seq(min(data$logD), max(data$logD), by=0.05)
+  XNew = data.frame(logD=(predDistance),
+                    logD2=(predDistance)^2)
+  p = predict(model,
+              newmods=as.matrix(XNew),
+              addx=TRUE, level=95)
+  predictions = data.frame(preds=p$pred,
+                           ci.ub=p$ci.ub,
+                           ci.lb=p$ci.lb,
+                           Dist=predDistance)
+  # Make plot
+  xmax = max(data$logD)
+  limy = max(c(abs(min(data$logRR)),abs(min(predictions$preds)),max(data$logRR),max(predictions$preds)))
+  data$pointsize1 = log10(1/data$VAR) - min(log10(1/data$VAR))
+  distPlot <- ggplot(data) +
+    ggtitle(title) +
+    geom_point(aes(logD,logRR), shape=20, color='slateblue4', alpha=0.1, size=data$pointsize1) +
+    ylab(expression(LRR^Delta)) +
+    xlab("Distance to infrastructure (m)") +
+    scale_x_continuous(breaks=c(-1,0,1,2,3), labels=c('0.1','1','10','100','1000'), limits=c(0,xmax)) +
+    geom_hline(yintercept=0, size=0.8, linetype="dashed", color="dark gray") +
+    geom_ribbon(data=predictions, aes(x=Dist, ymin=ci.lb, ymax=ci.ub), fill="grey50",linetype="blank", alpha=0.6) +
+    geom_line(data=predictions, aes(Dist,preds)) +
+    scale_y_continuous(breaks=c(-7.5,-5,-2.5,0,2.5,5,7.5), lim=c(-limy,limy)) +
+    annotate("text", x=0.60 * xmax, y=-limy + limy / 3.5,
+             size=8/3, hjust=0, vjust=0.25,
+             label=as.expression(bquote(italic(R[m]^2) == .(R2[1]))),
+             parse=TRUE) +
+    annotate("text", x=0.60 * xmax, y=-limy,
+             size=8/3, hjust=0, vjust=0.25,
+             label=as.expression(bquote(italic(R[c]^2) == .(R2[2]))),
+             parse=TRUE) +
+    theme_bw() +
+    science_theme
+  return(distPlot)
 }
 
