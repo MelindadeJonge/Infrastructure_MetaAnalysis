6a1ae8c3a573911fcecc31cec155316907eedca1
MelindadeJonge <melindadejonge@gmail.com>
2022-09-21 23:10:29 +0200

    Script for model selection added

---
+++ R/ModelSelection.R
@@ -0,0 +1,80 @@
+# Author: Melinda de Jonge
+# Date: 20-09-2022
+# Department of Environmental Science, Radboud University Nijmegen
+# e-mail: melinda.dejonge@ru.nl
+
+## -------------------------------------------------------------------------------------------------
+#' 
+##--------------------------------------------------------------------------------------------------
+
+library(metafor)
+library(doParallel)
+library(foreach)
+
+source(file.path('R','Functions.R'))
+source(file.path('R','FixedEffects.R'))
+
+ncores = 4
+
+# Load prepared data
+load(file.path('Data','Processed','GroupedData_main.RData'))
+load(file.path('Data','Processed','VarCovarMatrices_main.RData'))
+
+## ----- Model Selection & best models -------------------------------------------------------------
+REList = list(Mammals=list(~1|ID, ~1|Order/SpeciesCode, ~1|Source/Study),
+              Birds=list(~1|ID, ~1|Order/SpeciesCode, ~1|Source/Study),
+              Reptiles=list(~1|ID, ~1|Family/SpeciesCode, ~1|Source/Study),
+              Amphibians=list(~1|ID, ~1|Family/SpeciesCode, ~1|Source/Study))
+
+FEList_combined = list(FEMB, FEMB, FERep, FEAmph)
+
+for(group in 3:length(names(DataList))) {
+  print(paste0('Starting fixed effects selection for ',names(DataList)[group]))
+  FEList = FEList_combined[[group]]
+
+  cl = parallel::makeCluster(ncores)
+  registerDoParallel(cl)
+
+  Results <- foreach(i=1:length(FEList), .combine='rbind') %dopar% {
+    library(metafor)
+    FixedEffects = Reduce(paste, deparse(FEList[[i]][[1]]))
+    stats=rep(NA, 13)
+    try({
+      model <- rma.mv(logRR, mods=FEList[[i]][[1]],
+                      V=VarCovarList[[group]],
+                      random=REList[[group]],
+                      data=DataList[[group]],
+                      method="ML")
+      FitStats = model$fit.stats
+      R2 = R2.func(model)
+      stats = c(FitStats['AICc','ML'],FitStats['BIC','ML'],
+                model$QM,model$QMp,model$QE,model$QEp,
+                round(model$sigma2,4),round(R2,4))
+    }, silent=FALSE)
+    c(FixedEffects,stats)
+  }
+  stopImplicitCluster()
+  parallel::stopCluster(cl)
+
+  Results = data.frame(Results)
+  colnames(Results) = c('FixedEffects','AICc','BIC','Qm','Qmp','QE','QEp',
+                        'Sigma2_ID','Sigma2_Order_Family','Sigma2_Species',
+                        'Sigma2_Source','Sigma2_Study','R2m','R2c')
+  Results$AICc = as.numeric(Results$AICc)
+  Results = Results[order(Results$AICc), ]
+  Results$dAICc = Results$AICc - Results$AICc[1]
+  write.csv(Results, file=file.path('Output',
+                                    paste0('ModelSelectionAICc_',names(DataList)[group],'.csv')))
+
+  # Refit best model using REML and save in 'Models' directory
+  final_model <- rma.mv(logRR,mods=formula(Results$FixedEffects[1]),
+                  V=VarCovarList[[group]],
+                  random=REList[[group]],
+                  data=DataList[[group]],
+                  method="REML")
+  save(final_model,file=file.path('Output','Models',
+                                  paste0('FinalModel',names(DataList)[group],'.RData')))
+
+}
+
+
